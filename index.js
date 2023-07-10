2503fd10f002b606d1c5f364d5273baa4dc39b4c
Deniz Sarıkaş <75947870+denizsarikas@users.noreply.github.com>
2023-07-10 09:55:19 +0300

    added filter buttons, style and active class

--- index.js
+++ index.js
@@ -1,36 +1,59 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    var countries = [];
+document.addEventListener("DOMContentLoaded", () => {
+    let countries = [];
 
     fetch("https://restcountries.com/v2/all")
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
+        .then(response => response.json())
+        .then(data => {
             countries = data;
             showCountries(countries);
         })
-        .catch(function(error) {
+        .catch(error => {
             console.log("Hata:", error);
         });
 
-    var searchInput = document.getElementById("search-input");
-    searchInput.addEventListener("input", function() {
-        var searchValue = searchInput.value.toLowerCase();
-        var filteredCountries = countries.filter(function(country) {
-            return country.name.toLowerCase().includes(searchValue);
-        });
+    const searchInput = document.getElementById("search-input");
+    searchInput.addEventListener("input", () => {
+        const searchValue = searchInput.value.toLowerCase();
+        const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(searchValue));
         showCountries(filteredCountries);
     });
 
-    function showCountries(countriesToShow) {
-        var countriesList = document.getElementById("countries-list");
+    const filterButtons = document.querySelectorAll(".filter-button");
+    filterButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            // Önce tüm buttonlardaki aktif durumu kaldır
+            filterButtons.forEach(btn => btn.classList.remove("active"));
+
+            // Tıklanan butona aktif durumunu ekle
+            button.classList.add("active");
+
+            const region = button.dataset.region;
+            const filteredCountries = countries.filter(country => country.region === region);
+            showCountries(filteredCountries);
+        });
+    });
+
+    const showAllButton = document.getElementById("show-all-button");
+    showAllButton.addEventListener("click", () => {
+        showCountries(countries);
+    });
+
+    const showCountries = (countriesToShow) => {
+        const countriesList = document.getElementById("countries-list");
         countriesList.innerHTML = "";
 
-        countriesToShow.forEach(function(country) {
-            var countryItem = document.createElement("li");
-            countryItem.textContent = country.name;
+        countriesToShow.forEach(country => {
+            const countryItem = document.createElement("li");
+            countryItem.classList.add("country-item");
+            const countryName = document.createElement("span");
+            countryName.classList.add("country-name");
+            countryName.textContent = country.name;
+            const countryRegion = document.createElement("span");
+            countryRegion.classList.add("country-region");
+            countryRegion.textContent = country.region;
+            countryItem.appendChild(countryName);
+            countryItem.appendChild(countryRegion);
             countriesList.appendChild(countryItem);
         });
-    }
+    };
 });
